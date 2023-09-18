6cdd9654ff12789fec7d2b62583e9377ad33411d
hanhosi <leehanbyeols2@gmail.com>
2023-09-18 17:50:32 +0900

    4-1 commit

--- 4-1.py
+++ 4-1.py
@@ -89,29 +89,153 @@ print(*enumerate(a))"""
 
 # zip
 
-a = [1, 2,3]
+"""a = [1, 2,3]
 b = [4, 5, 6]
-print(*zip(a,b))
+print(*zip(a,b))"""
 
 
 # any, all
 
-a = [True, True, False]
+"""a = [True, True, False]
 a = [True, True, True]
 print(any(a))
 print(all(a))
 print(all(b))
+"""
 
 
 # format
 
-a = 20 
+"""a = 20 
 b = 23
 c = "a는 {}, b는 {}".format(a, b, "python")
 
-print(c)
+print(c)"""
 
-a = 3
-print(globals())
-print(locals())
+"""a = 3
+# print(globals())
+# print(locals())"""
 
+"""print(dir(a))
+
+print(callable(a))"""
+
+
+#lambda
+
+""" add = lambda a, b : a + b
+print(add(2, 3))
+
+
+add = lambda a, b : a + b
+sub = lambda a, b : a - b
+mul = lambda a, b : a * b
+div = lambda a, b : a / b
+
+print(add(2, 3))
+print(sub(2, 3))
+print(mul(2, 3))
+print(div(2, 3)) """
+
+
+# 사용자 정의
+
+"""def add_numbers(x, y):
+    return x + y
+
+# 함수 호출
+result = add_numbers(4, 5)
+print(result)"""
+
+
+"""def greet(name):
+    print(name)
+    
+    print("Hello, " + name + ". How are you?")
+
+greet("최서연")"""
+
+
+# 매개변수, 인자
+
+""" def add(a, b) : 
+	print(a + b)
+
+def sub(a, b) :
+	return a - b
+
+def mul() :
+	return 2 * 4
+
+def div() :
+	print(4 / 2)
+
+add(3, 5)
+print(sub(3, 5))
+print(mul())
+div() """
+
+
+# 입력값 홀/짝수
+
+"""def is_even(n) :
+    if  n % 2 == 0:
+        print("짝수")
+    else :
+        print("홀수")
+        
+num = input("숫자를 입력하세요 : ")
+
+is_even(int(num))"""
+
+
+# 문자열 반전
+
+"""def reverse_string(msg) :
+    return msg[::-1]
+
+in_str = input("문자열 : ")
+print(reverse_string(in_str))"""
+
+
+# 두수를 입력받아 사칙연산
+
+"""def add(a, b) :
+    return int(a) + int(b)
+def sub(a, b) :
+    return int(a) - int(b)
+def mul(a, b) :
+    return int(a) * int(b)
+def div(a, b) :
+    return int(a) / int(b)
+
+a = input("a를 입력하세요")
+b = input("b를 입력하세요")
+
+print("더하기 : ", add(a, b))
+print("삐기 : ", sub(a, b))
+print("곱하기 : ", mul(a, b))
+print("나누기 : ", div(a, b))"""
+
+
+"""def calc(a, b) :
+    print(int(a) + int(b))
+    print(int(a) - int(b))
+    print(int(a) * int(b))
+    print(int(a) / int(b))
+    
+a = input("a를 입력하세요")
+b = input("b를 입력하세요")
+
+calc(a, b)"""
+
+# 5개 수 입력
+def sum_num(num) :
+    return sum(num)
+
+nums = []
+
+for i in range(1, 6) :
+    innum = int(input(f"{i} 번쨰 숫자 입력 : "))
+    
+print(sum_num(nums))
